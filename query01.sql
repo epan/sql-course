bf540020ebb417b12ed68eeaa3bdd7bac5c3727f
Eric Pan <ericpanbot@gmail.com>
2015-07-24 03:11:09 -0700

    Brainbuster 3 complete

--- query01.sql
+++ query01.sql
@@ -6,7 +6,36 @@ FROM film
 -- Brainbuster #1
 -- Find customers by first name, last name, and email
 -- who are customers of store 2
-SELECT first_name, last_name, email, store_id
-FROM customer
-WHERE store_id = 2
+SELECT
+  first_name, last_name, email, store_id
+FROM
+  customer
+WHERE
+  store_id = 2
+;
+
+-- Trying count()
+SELECT count(title), rental_rate
+FROM film
+;
+
+-- Group by prices
+SELECT
+  count(title), rental_rate
+FROM
+  film
+GROUP BY
+  rental_rate
+;
+
+-- Brainbuster 2
+-- Which rating do we have the most films in?
+-- The find out which rating is most prevalent in each price
+-- Use only 1 query
+SELECT
+  count(title), rating, rental_rate
+FROM
+  film
+GROUP BY
+  rental_rate, rating
 ;
