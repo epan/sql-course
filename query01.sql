97f3694adefb93e1043b0937fd3713e466950bd7
Eric Pan <ericpanbot@gmail.com>
2015-07-26 22:49:46 -0700

    Working query for rentals by title

--- query01.sql
+++ query01.sql
@@ -39,3 +39,30 @@ FROM
 GROUP BY
   rental_rate, rating
 ;
+
+-- Brainbuster 3
+-- List of every single film, with the name, category, and language of it
+SELECT
+  film.title, category.name, language.name
+FROM
+  film, film_category, category, language
+WHERE
+  film.film_id = film_category.film_id
+  AND
+  film_category.category_id = category.category_id
+  AND
+  film.language_id = language.language_id
+;
+
+-- Exercise on rentals by title
+SELECT
+  film.title, count(rental.rental_id)
+FROM
+  film, inventory, rental
+WHERE
+  film.film_id = inventory.film_id
+  and
+  rental.inventory_id = inventory.inventory_id
+GROUP BY
+  film.title
+;
